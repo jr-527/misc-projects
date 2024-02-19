16ae6f40fc110413cf74ceb5e79ede1d1ebf6777
jr-527 <108962469+jr-527@users.noreply.github.com>
2024-02-19 03:51:11 -0800

    Made my own rounding function

--- terminal_plot.py
+++ terminal_plot.py
@@ -31,8 +31,31 @@ plot(x*1e-9, y, stem=True)
 horizontal_bar_chart(x*1e-9, y**2)
 horizontal_bar_chart(y**2)
 
-License: This file may be freely used, reproduced, distributed, etc.
-         Go wild with it.
+License:
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
 '''
 # print(x) is valid in Python 2 - it's equivalent to print (x)
 try: # In case you somehow don't have the os module
@@ -41,28 +64,188 @@ except:
     pass
 
 _dummy = object()
-# modified from https://stackoverflow.com/a/63947444
-def _format_float(x, width=8, left=True):
-    maxnum = int('9'*(width - 1))
-    minnum = -int('9'*(width - 2))
-    if x > minnum and x < maxnum and (x == 0 or abs(x) > 1e-3):
-        if not left:
-            o = ('{:' + str(width-1) + '}').format(x)
-        else:
-            o = ('{:<' + str(width - 1) + '}').format(x)
-        if len(o) > width:
-            o = str(round(x, width - 1 - str(x).index('.')))
-            if len(o) < width:
-                o+=(width-len(o))*'0'
+_NEG_OF = 'negative overflow'
+_OF = 'overflow'
+_OK = 'okay'
+
+def _round_to_1(x):
+    if x < -0.5: return ('0', _NEG_OF)
+    if x <= 0.5: return ('0', _OK)
+    if x >= 9.5: return ('9', _OF)
+    return (str(int(round(x))), _OK)
+
+def _round_to_2(x, leading_zero=False):
+    if x <= -9.5: return ('-9', _NEG_OF)
+    if x < -0.5: return (str(int(round(x))), _OK)
+    if x < 0: return ('-0', _OK)
+    if not leading_zero and x < 1:
+        if 0.95 <= x: return ('1', _OK)
+        if 0.05 >= x: return ('0', _OK)
+        return (str(int(round(10*x))/10.0)[1:3], _OK)
+    if x >= 99.5: return ('99', _OF)
+    return (str(int(round(x))), _OK)
+
+def _round_to_3(x, leading_zero=False):
+    if x < 0:
+        out = _round_to_2(-x, leading_zero)
+        if out[1] == _OF: return ('-99', _NEG_OF)
+        return ('-' + out[0], _OK)
+    if not leading_zero and x < 1:
+        if 0.995 <= x: return ('1', _OK)
+        if 0.005 >= x: return ('0', _OK)
+        return (str(round(x,2))[1:4], _OK)
+    if x < 1:
+        return ('0' + _round_to_2(x, False)[0], _OK)
+    if x < 9.95: return (str(round(x,1)), _OK)
+    if x < 999.5: return (str(int(round(x))), _OK)
+    if 999.5 <= x and x < 9.5e9:
+        out = format(x, '1e')
+        first_digit = int(round(float(out[:3])))
+        last_digit = int(out[-1])
+        if first_digit == 10:
+            last_digit += 1
+            first_digit = 1
+        return (str(first_digit) + 'e' + str(last_digit), _OK)
+    return ('9e9', _OF)
+
+def _round_to_n(x, n, leading_zero=False):
+    if n >= 25:
+        return (str(x), _OK)
+    if n == 3:
+        return _round_to_3(x, leading_zero)
+    if x < 0:
+        out, status = _round_to_n(-x, n-1, leading_zero)
+        status = status if status == _OK else _NEG_OF
+        return ('-' + out, status)
+    if x <= float('0.5e-' + '9'*(n-3)):
+        return ('0', _OK)
+    if x < float('1.5e-' + '9'*(n-3)):
+        return ('1e-' + '9'*(n-3), _OK)
+    if x < .001:
+        mantissa, exponent = format(x, '.18e').split('e')
+        exponent = 'e' + str(int(exponent))
+        mantissa = str(round(float(mantissa), max(0,n-len(exponent)-2)))
+        mantissa = mantissa[:n-len(exponent)]
+        if mantissa[-1] == '.':
+            mantissa = mantissa[:-1]
+        return (mantissa + exponent, _OK)
+    threshold_str = '9'*n + '5'
+    for i in range(n+1):
+        threshold = float(threshold_str[:i] + '.' + threshold_str[i:])
+        if x < threshold:
+            out = ''
+            if i == 0:
+                if not leading_zero:
+                    out = str(round(x,n-i-1))[1:]
+                else:
+                    out = str(round(x,n-i-2))
+            if i == n:
+                out = str(int(round(x)))[:n]
+            else:
+                out = str(round(x,n-i-1))[:n]
+                if out[-1] == '.':
+                    #out = str(round(x,n-i-1))[:n-1]
+                    out = out[:-1]
+            out = out[:n]
+            if out[-1] == '.':
+                out = out[:-1]
+            return (out, _OK)
+    if x < float('9.5e' + '9'*(n-2)):
+        mantissa, exponent = format(x, '.17e').split('e')
+        exponent = 'e' + str(int(exponent))
+        mantissa = str(round(float(mantissa), max(0,n-len(exponent)-2)))
+        mantissa = mantissa[:n-len(exponent)]
+        if mantissa[-1] == '.':
+            mantissa = mantissa[:-1]
+        return (mantissa + exponent, _OK)
+    return (float('9e' + '9'*(n-2)), _OF)
+
+def _round_to_width(x, width=8, align='left', overflow='saturate', leading_zero=False, underflow='zero'):
+    '''
+    Returns a string of length width representing a number as close to x as
+    possible (prefers '1.2e9' over '123e7' because it's easier to understand)
+    x: The number to round
+    width: The string's width
+    align: If 'left', align the string on the left, ie '3.14   '.
+           Otherwise align the string on the right, ie '   3.14'
+    overflow: Either 'saturate', 'exception', 'inf', 'nan', or 'word'
+             Describes what to do if the number is too big to represent in the
+             desired number of characters.
+        'saturate': Give the closest possible number, so
+                    round_to_width(1e20, 3) gives '9e9'
+        'exception': Raise an exception.
+        'inf': Return 'inf' or '-inf' (replaced by 'exception' if width < 4)
+        'nan': Return 'NaN' or '-NaN' (replaced by 'exception' width < 4)
+        'word': Returns text that says something along the lines of 'overflow'
+                that won't parse as a number.
+    underflow: Either 'zero', 'saturate', 'exception', or 'word'
+               Describes what to do if the number is too close to zero to
+               represent in the desired number of characters.
+        'zero': Return '0'
+        'saturate': Give the smallest possible number of the correct sign, so
+                    round_to_width(1e-99, 4) gives '1e-9'.
+                    Replaced by 'exception' if width < 2.
+        'exception': Raise an exception.
+        'word': Returns text that says something along the lines of 'eps'
+                that won't parse as (part of) a number (so no 'e').
+                Replaced by 'exception' if width < 3.
+    leading_zero: If True, 0.5 will be formatted as '0.5', otherwise '.5'
+    '''
+    width = int(width)
+    if width == 0:
+        # returning '' is a silent failure, which we don't want.
+        raise ValueError('width must be > 0')
+    overflow = overflow.lower()
+    underflow = underflow.lower()
+    if underflow in ('raise', 'except'):
+        underflow = 'exception'
+    if (underflow == 'word' and width < 3) or (underflow == 'word' and width < 2):
+        underflow = 'exception'
+    if overflow in ('raise', 'except') or (width < 4 and overflow in ('inf', 'nan')):
+        overflow = 'exception'
+    if overflow not in ('exception', 'saturate', 'inf', 'nan', 'word'):
+        raise ValueError("overflow must be one of 'exception', 'saturate', 'inf', 'nan', 'word'")
+    if underflow not in ('zero', 'saturate', 'exception', 'word'):
+        raise ValueError("underflow must be one of 'zero', 'saturate', 'exception', 'word'")
+    out, status = '', ''
+    if width == 1:
+        out, status = _round_to_1(x)
+    elif width == 2:
+        out, status = _round_to_2(x, leading_zero)
     else:
-        for n in range(max(width, 5) - 5, 0, -1):
-            fill = '.' + str(n) + 'e'
-            o = ('{:' + fill + '}').format(x).replace('+0', '+').replace('-0', '-')
-            if len(o) == width:
-                break
-        else:
-            raise ValueError('Number is too large to fit in ' + str(width) + ' characters', x)
-    return o
+        out, status = _round_to_n(x, width, leading_zero)
+    if out == '0' and x != 0: # this branch always has status == _OK
+        # 'zero' -> do nothing
+        if underflow == 'exception':
+            raise ValueError('Underflow')
+        if underflow == 'saturate':
+            # the smallest floating point number is around 1e-308,
+            # so we don't have to worry about 1e-999.
+            temp_arr = [('1', '1'), ('.1', '1'), ('.01', '0.1'),
+                        ('1e-9', '1e-9'), ('1e-99', '1e-99')]
+            if x > 0: out = temp_arr[width-1][leading_zero]
+            else: out = '-' + temp_arr[width-2][leading_zero]
+        elif underflow == 'word':
+            out = 'eps'
+            if x < 0:
+                out = '-ep' if width == 3 else '-eps'
+    fmt = '<' if align=='left' else '>'
+    out = format(out, fmt+str(int(width)))
+    if status != _OK:
+        # saturate -> do nothing
+        if overflow == 'exception':
+            raise ValueError(status)
+        if overflow == 'inf':
+            out = 'inf' if status == _OF else '-inf'
+        elif overflow == 'nan':
+            status = 'NaN' if status == _OF else '-NaN'
+        elif overflow == 'word':
+            if width < 5:
+                state = 1 if status == _OF else 0
+                return [('N', 'P'), ('-N', 'OF'), ('-OF', 'OVF'), ('-OVF', 'OVER')][width-1][state]
+            out = 'OVERFLOW' if status == _OF else '-OVERFLOW'
+            out = out[:min(len(out), width)]
+    return out
 
 def horizontal_bar_chart(x, y=None, dummy_arg=_dummy, width=None, return_text=False):
     '''
@@ -124,11 +307,11 @@ def horizontal_bar_chart(x, y=None, dummy_arg=_dummy, width=None, return_text=Fa
         if labels is not None:
             out = str(labels[i])[:15]
             if index_length == 15 and not isinstance(labels[i], str):
-                out = _format_float(labels[i], 15, left=False)
+                out = _round_to_width(labels[i], 15, align='right')
             out = out.rjust(index_length, ' ')
         else:
             out = str(i+start).rjust(index_length, ' ')
-        out += '|' + '*'*val + ' ' + _format_float(v, 12)
+        out += '|' + '*'*val + ' ' + _round_to_width(v, 12)
         text += out + '\n'
     if return_text:
         return text
@@ -138,19 +321,19 @@ def horizontal_bar_chart(x, y=None, dummy_arg=_dummy, width=None, return_text=Fa
 def plot(x, y=None, dummy_arg=_dummy, width=None, height=None, stem=False, return_text=False):
     '''
     This function makes a plot using plain text, ie
-    >>> plot(np.cos(np.arange(40)/2), width=40, height=9)
+    >>> plot([math.cos(i*.5) for i in range(41)], width=40, height=9)
     __________________________________________ 1.0
-    |**          **           **          ** |
-    |           *  *         *           *  *| 0.556184
-    |  *                    *   *            |
-    |          *    *            *      *    |
-    |   *                  *                 | -0.10954
-    |         *      *            *    *     |
-    |    *            *   *                  |
-    |     *  *           *         *  *      | -0.77526
-    |      **          **           **       |
+    |**          **          **          **  |
+    |           *  *        *           *  * | 0.556184
+    |  *                   *   *            *|
+    |          *    *           *      *     |
+    |   *                 *                  | -0.10954
+    |         *      *           *    *      |
+    |    *            *  *                   |
+    |     *  *           *        *  *       | -0.77526
+    |      **          **          **        |
     -+------------+------------+------------+- -0.9972
-    0          13.0         26.0         39
+    0          13.3333      26.6667      40
 
     x: A list (or numpy array or whatever) of 'shape' (n,) or (n,2).
     y: Numeric array of shape (n,), optional.
@@ -163,7 +346,7 @@ def plot(x, y=None, dummy_arg=_dummy, width=None, height=None, stem=False, retur
     height: None or a positive integer. The number of rows in the plot box.
         If width or height are missing, this function tries to auto-size the
         plot box to fit the terminal window, otherwise using default values
-        of width=70, height=25.
+        of width=69, height=22 to fit in an 80x24 terminal.
     stem: If True, this shades the area under the curve.
           Good for histograms, bar charts, and discrete data.
     return_text: If False, this prints to console and returns None. If True,
@@ -176,12 +359,12 @@ def plot(x, y=None, dummy_arg=_dummy, width=None, height=None, stem=False, retur
         try:
             height = os.get_terminal_size()[1]-4
         except:
-            height = 25
+            height = 22
     if width is None:
         try:
             width = os.get_terminal_size()[0]-12
         except:
-            width = 70
+            width = 69
         if width <= 10:
             height -= 2
     # we want:
@@ -213,8 +396,8 @@ def plot(x, y=None, dummy_arg=_dummy, width=None, height=None, stem=False, retur
             for i in filter(between, range(0, height)):
                 arr[i][x_coord] = max(arr[i][x_coord], 1)
         arr[y_coord][x_coord] += 1
-    # print('_'*(width+2), _format_float(y_max, 8))
-    text += '_'*(width+2) + ' ' + _format_float(y_max, 8) + '\n'
+    # print('_'*(width+2), _round_to_width(y_max, 8))
+    text += '_'*(width+2) + ' ' + _round_to_width(y_max, 8) + '\n'
     for i in range(len(arr)):
         j = len(arr)-i-1
         row = arr[j]
@@ -228,7 +411,7 @@ def plot(x, y=None, dummy_arg=_dummy, width=None, height=None, stem=False, retur
                 string += '#'
         string += '|'
         if (i % 3 == 1):# or (i == len(arr)-1):
-            string += ' ' + _format_float(y_min + y_range * j/(1.0*len(arr)), 8)
+            string += ' ' + _round_to_width(y_min + y_range * j/(1.0*len(arr)), 8)
         # print(string)
         text += string + '\n'
     fractions = [0, 0.25, 0.5, 0.75, 1]
@@ -245,7 +428,7 @@ def plot(x, y=None, dummy_arg=_dummy, width=None, height=None, stem=False, retur
     # try:
     #     if ascii:
     #         raise Exception
-    #     out = list('\u203e'*(width+2) + ' ' + _format_float(y_min, 8))
+    #     out = list('\u203e'*(width+2) + ' ' + _round_to_width(y_min, 8))
     #     for fraction in fractions:
     #         i = 1+int(round(fraction*(width-1)))
     #         out[i] = '^'
@@ -260,25 +443,22 @@ def plot(x, y=None, dummy_arg=_dummy, width=None, height=None, stem=False, retur
     # print(''.join(out))
     text += ''.join(out) + '\n'
     if len(fractions) > 0:
-        out = _format_float(x_min, 7)
+        out = _round_to_width(x_min, 7)
         prev = 5
         for i, fraction in spots[1:]:
             prev = len(out)
             out += ' '*(i-3-prev)
-            out += _format_float(x_min + fraction*x_range, 7)
+            out += _round_to_width(x_min + fraction*x_range, 7)
         # print(out)
         text += out
     else:
-        # text += f'min x:{_format_float(x_min, 7)}\n'
-        text += 'min x:' + _format_float(x_min, 7) + '\n'
-        text += 'max x:' + _format_float(x_max, 7)
-        # text += f'max x:{_format_float(x_max, 7)}'
-        # print('min x:', _format_float(x_min, 7))
-        # print('max x:', _format_float(x_max, 7))
+        # text += f'min x:{_round_to_width(x_min, 7)}\n'
+        text += 'min x:' + _round_to_width(x_min, 7) + '\n'
+        text += 'max x:' + _round_to_width(x_max, 7)
+        # text += f'max x:{_round_to_width(x_max, 7)}'
+        # print('min x:', _round_to_width(x_min, 7))
+        # print('max x:', _round_to_width(x_max, 7))
     if return_text:
         return text + '\n'
     print(text)
     return None
-
-# horizontal_bar_chart([x**2 for x in range(5)], width=16)
-# plot([x**2 for x in range(5)], width=16)
